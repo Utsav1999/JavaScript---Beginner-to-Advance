56e33036e87973ba7fc5f7d6d29313f5216bd746
Utsav1999 <utsavakash123@gmail.com>
2020-07-04 23:20:33 +0530

    JavaScript Basics

---
+++ JS Basics/script _data_types_and_variables.js
@@ -0,0 +1,57 @@
+/*
+    Variables and Datatypes
+*/
+
+/*
+//console.log("Hello World!!!");
+var firstName = 'Utsav Akash';
+console.log(firstName);
+var lastName = 'Naskar';
+var age = 21;
+var fullAge = true;
+//console.log(fullAge);
+var job;
+//console.log(job);
+job = 'Teacher';
+console.log(job);
+*/
+
+/*
+    Variable mutation and type coercion
+*/
+
+//type coercion
+var firstName = 'Utsav Akash';
+var age = 21;
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job='teacher';
+isMarried=false;
+
+console.log(firstName+ ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//variable mutation
+
+age = 'twenty one';
+job = 'driver';
+alert(firstName+ ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' +lastName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
